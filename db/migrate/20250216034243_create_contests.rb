f27790fade175cfd4acc3b29b62365a6745b0933
Yevhenii Hurin <evgeny.gurin@gmail.com>
2025-02-16 05:47:07 +0200

    Add Contest model

---
+++ db/migrate/20250216034243_create_contests.rb
@@ -0,0 +1,15 @@
+class CreateContests < ActiveRecord::Migration[8.0]
+  def change
+    create_table :contests do |t|
+      t.integer :year
+      t.references :host_country, null: false, foreign_key: true
+      t.string :host_city
+      t.string :slogan
+      t.datetime :grand_final_date
+      t.datetime :first_semifinal_date
+      t.datetime :second_semifinal_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_16_031345) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_16_034243) do
+  create_table "contests", force: :cascade do |t|
+    t.integer "year"
+    t.integer "host_country_id", null: false
+    t.string "host_city"
+    t.string "slogan"
+    t.datetime "grand_final_date"
+    t.datetime "first_semifinal_date"
+    t.datetime "second_semifinal_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["host_country_id"], name: "index_contests_on_host_country_id"
+  end
+
   create_table "countries", force: :cascade do |t|
     t.string "name"
     t.string "code"
@@ -21,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_16_031345) do
 
   create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
   end
+
+  add_foreign_key "contests", "host_countries"
 end
