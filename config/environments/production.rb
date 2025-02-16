3a78784f24c57f15014438d67515ae2e80c530a2
Yevhenii Hurin <evgeny.gurin@gmail.com>
2025-02-16 23:43:32 +0200

    Remove host restriction on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts = [
-    "cg-esc.fly.dev",
-    "esc.hobl.in"
-  ]
+  # config.hosts = [
+  #   "cg-esc.fly.dev",
+  #   "esc.hobl.in"
+  # ]
 end
